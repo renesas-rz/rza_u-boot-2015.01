38a209a38d039549d622ca81c381d81554ca4456
Chris Brandt <chris.brandt@renesas.com>
2015-06-18 20:36:03 -0400

    Add functions for configuring I/O pins

--- board/renesas/rskrza1/rskrza1.c
+++ board/renesas/rskrza1/rskrza1.c
@@ -22,6 +22,108 @@
 
 //#define DEBUG
 
+/* Port Function Registers */
+#define PORTn_base 0xFCFE3000
+#define Pn(n)	(PORTn_base + 0x0000 + n * 4)	/* Port register R/W */
+#define PSRn(n)	(PORTn_base + 0x0100 + n * 4)	/* Port set/reset register R/W */
+#define PPRn(n)	(PORTn_base + 0x0200 + n * 4)	/* Port pin read register R */
+#define PMn(n)	(PORTn_base + 0x0300 + n * 4)	/* Port mode register R/W */
+#define PMCn(n)	(PORTn_base + 0x0400 + n * 4)	/* Port mode control register R/W */
+#define PFCn(n)	(PORTn_base + 0x0500 + n * 4)	/* Port function control register R/W */
+#define PFCEn(n)	(PORTn_base + 0x0600 + n * 4)	/* Port function control expansion register R/W */
+#define PNOTn(n)	(PORTn_base + 0x0700 + n * 4)	/* Port NOT register W */
+#define PMSRn(n)	(PORTn_base + 0x0800 + n * 4)	/* Port mode set/reset register R/W */
+#define PMCSRn(n)	(PORTn_base + 0x0900 + n * 4)	/* Port mode control set/reset register R/W */
+#define PFCAEn(n)	(PORTn_base + 0x0A00 + n * 4)	/* Port Function Control Additional Expansion register R/W */
+#define PIBCn(n)	(PORTn_base + 0x4000 + n * 4)	/* Port input buffer control register R/W */
+#define PBDCn(n)	(PORTn_base + 0x4100 + n * 4)	/* Port bi-direction control register R/W */
+#define PIPCn(n)	(PORTn_base + 0x4200 + n * 4)	/* Port IP control register R/W */
+
+enum pfc_pin_alt_mode {ALT1=1, ALT2, ALT3, ALT4, ALT5, ALT6, ALT7, ALT8};
+enum pfc_pin_gpio_mode {GPIO_OUT=0, GPIO_IN=1};
+
+const u32 alt_settings[9][3] = {
+	/* PFCAEn, PFCEn, PFCn */
+	{0,0,0},/* dummy */
+	{0,0,0},/* 1st alternative function */
+	{0,0,1},/* 2nd alternative function */
+	{0,1,0},/* 3rd alternative function */
+	{0,1,1},/* 4th alternative function */
+	{1,0,0},/* 5th alternative function */
+	{1,0,1},/* 6th alternative function */
+	{1,1,0},/* 7th alternative function */
+	{1,1,1},/* 8th alternative function */
+};
+
+/* Arguments:
+   n = port(1-11)
+   b = bit(0-15)
+   d = direction('GPIO_IN','GPIO_OUT')
+*/
+void pfc_set_gpio(u8 n, u8 b, u8 d)
+{
+	*(u32 *)PMCSRn(n) = 1UL<<(b+16);	// Pin as GPIO
+	*(u32 *)PSRn(n) = 1UL<<(b+16) | (u32)d<< b;	// Set direction
+}
+
+/* Arguments:
+    n = port number (P1-P11)
+    b = bit number (0-15)
+    alt = Alternative mode ('ALT1'-'ALT7')
+    inbuf =  Input buffer (0=disabled, 1=enabled)
+    bi = Bidirectional mode (0=disabled, 1=enabled)
+*/
+void pfc_set_pin_function(u16 n, u16 b, u16 alt, u16 inbuf, u16 bi)
+{
+	u16 value;
+
+	/* Set PFCAEn */
+	value = *(u16 *)PFCAEn(n);
+	value &= ~(1UL<<b); // clear
+	value |= (alt_settings[alt][0] & 1UL) << b; // set(maybe)
+	*(u16 *)PFCAEn(n) = value;
+
+	/* Set PFCEn */
+	value = *(u16 *)PFCEn(n);
+	value &= ~(1UL<<b); // clear
+	value |= (alt_settings[alt][1] & 1UL) << b; // set(maybe)
+	*(u16 *)PFCEn(n) = value;
+
+	/* Set PFCn */
+	value = *(u16 *)PFCn(n);
+	value &= ~(1UL<<b); // clear
+	value |= (alt_settings[alt][2] & 1UL) << b; // set(maybe)
+	*(u16 *)PFCn(n) = value;
+
+	/* Set Pn */
+	/* Not used for alternative mode */
+	/* NOTE: PIP must be set to '0' for the follow peripherals and Pn must be set instead
+		<> Multi-function timer pulse unit
+		<> LVDS output interface
+		<> Serial sound interface
+	   For those, use this to set Pn: *(u32 *)PSRn(n) = 1UL<<(b+16) | direction<<(b);
+	*/
+
+	/* Set PIBCn */
+	value = *(u16 *)PIBCn(n);
+	value &= ~(1UL<<b); // clear
+	value |= inbuf << b; // set(maybe)
+	*(u16 *)PIBCn(n) = value;
+
+	/* Set PBDCn */
+	value = *(u16 *)PBDCn(n);
+	value &= ~(1UL<<b); // clear
+	value |= bi << b; // set(maybe)
+	*(u16 *)PBDCn(n) = value;
+
+	/* Alternative mode '1' (not GPIO '0') */
+	*(u32 *)PMCSRn(n) |= 1UL<<(b+16) | 1UL<<(b);
+
+	/* Set PIPCn so pin used for function '1'(not GPIO '0') */
+	*(u16 *)PIPCn(n) |= 1UL <<b;
+}
+
+
 int spi_flash_cmd(struct spi_slave *spi, u8 cmd, void *response, size_t len);
 struct spi_flash *spi_flash_probe(unsigned int bus, unsigned int cs,
 		unsigned int max_hz, unsigned int spi_mode);
@@ -45,6 +147,46 @@ int board_init(void)
 int board_early_init_f(void)
 {
 	rtc_reset();	/* to start rtc */
+
+	/* =========== Pin Setup =========== */
+	/* Specific for the RZ/H on the RSK board. Adjust for your board as needed. */
+
+	/* Serial Console */
+	pfc_set_pin_function(3, 0, ALT6, 0, 0);	/* P3_0 = TxD2 */
+	pfc_set_pin_function(3, 2, ALT4, 0, 0);	/* P3_2 = RxD2 */
+
+	/* QSPI (2nd SPI chip interface) */
+	pfc_set_pin_function(2, 12, ALT4, 0, 0);	/* P2_12 = SPBIO01_0 */
+	pfc_set_pin_function(2, 13, ALT4, 0, 0);	/* P2_13 = SPBIO11_0 */
+	pfc_set_pin_function(2, 14, ALT4, 0, 0);	/* P2_14 = SPBIO21_0 */
+	pfc_set_pin_function(2, 15, ALT4, 0, 0);	/* P2_15 = SPBIO31_0 */
+
+	/* RIIC Ch 3 */
+	pfc_set_pin_function(1, 6, ALT1, 0, 1);	/* P1_6 = RIIC3SCL (bi dir) */
+	pfc_set_pin_function(1, 7, ALT1, 0, 1);	/* P1_7 = RIIC3SDA (bi dir) */
+
+	/* Ethernet */
+	pfc_set_pin_function(1, 14, ALT4, 0, 0); /* P1_14 = ET_COL */
+	pfc_set_pin_function(5, 9, ALT2, 0, 0);	/* P5_9 = ET_MDC */
+	pfc_set_pin_function(3, 3, ALT2, 0, 1);	/* P3_3 = ET_MDIO (bi dir) */
+	pfc_set_pin_function(3, 4, ALT2, 0, 0);	/* P3_4 = ET_RXCLK */
+	pfc_set_pin_function(3, 5, ALT2, 0, 0);	/* P3_5 = ET_RXER */
+	pfc_set_pin_function(3, 6, ALT2, 0, 0);	/* P3_6 = ET_RXDV */
+	pfc_set_pin_function(2, 0, ALT2, 0, 0);	/* P2_0 = ET_TXCLK */
+	pfc_set_pin_function(2, 1, ALT2, 0, 0);	/* P2_1 = ET_TXER */
+	pfc_set_pin_function(2, 2, ALT2, 0, 0);	/* P2_2 = ET_TXEN */
+	pfc_set_pin_function(2, 3, ALT2, 0, 0);	/* P2_3 = ET_CRS */
+	pfc_set_pin_function(2, 4, ALT2, 0, 0);	/* P2_4 = ET_TXD0 */
+	pfc_set_pin_function(2, 5, ALT2, 0, 0);	/* P2_5 = ET_TXD1 */
+	pfc_set_pin_function(2, 6, ALT2, 0, 0);	/* P2_6 = ET_TXD2 */
+	pfc_set_pin_function(2, 7, ALT2, 0, 0);	/* P2_7 = ET_TXD3 */
+	pfc_set_pin_function(2, 8, ALT2, 0, 0);	/* P2_8 = ET_RXD0 */
+	pfc_set_pin_function(2, 9, ALT2, 0, 0);	/* P2_9 = ET_RXD1 */
+	pfc_set_pin_function(2, 10, ALT2, 0, 0);	/* P2_10 = ET_RXD2 */
+	pfc_set_pin_function(2, 11, ALT2, 0, 0);	/* P2_11 = ET_RXD3 */
+	//pfc_set_pin_function(4, 14, ALT8, 0, 0);	/* P4_14 = IRQ6 (ET_IRQ) */ /* NOTE: u-boot doesn't enable interrupts */
+
+
 	return 0;
 }
 
